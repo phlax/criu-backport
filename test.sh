daa793d71ca6561d6eec567a83bccbc878011a58
Ryan Northey <ryan@synca.io>
2018-05-08 10:58:12 +0100

    Find leaking fd dynamically

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-exec 6>&-
+
+
+# theres a leaking fd on travis - this seems very arbitrary, but not sure how
+# else to find/purge it
+FD=$(lsof -p $$ | grep "136,0" | tr -s ' ' | cut -d" " -f4  | sed 's/[^0-9]*//g')
+exec $FD>&-
+
 while :; do
     sleep 1
     date
