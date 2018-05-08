77b12e187922170461c136beaf36a210f9fe0a0a
Ryan Northey <ryan@synca.io>
2018-05-08 11:05:19 +0100

    debug

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-
-# theres a leaking fd on travis - this seems very arbitrary, but not sure how
-# else to find/purge it
-FD=$(lsof -p $$ | grep "136,0" | tr -s ' ' | cut -d" " -f4  | sed 's/[^0-9]*//g')
-exec $FD>&-
-
 while :; do
     sleep 1
     date
